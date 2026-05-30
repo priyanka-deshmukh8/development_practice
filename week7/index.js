69acd01e45df412b07e9744f9550794805f5caf1
priyanka-deshmukh8 <priyankadeshmukh.h@gmail.com>
2026-05-30 15:28:26 +0530

    auth

---
+++ week7/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { UserModel, TodoModel } = require('./db');
+
+const app = express();  
+
+app.use(express.json());
+
+app.post("/signup", function (req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+    
+    UserModel.insert({
+        name: name,
+        password: password,
+        email: email
+    })
+    res.json({
+        message: "You are signed up!"
+    });
+});
+
+app.post("/login", function (req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    const user = UserModel.findOne({
+        email: email,
+        password: password 
+    });
+    if (user) {
+        res.json({
+            message: "You are logged in!"
+        });
+    } else {
+        res.json({
+            message: "Invalid email or password"
+        });
+    }
+});
+app.listen(3000);
